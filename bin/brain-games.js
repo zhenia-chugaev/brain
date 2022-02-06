8f521e240dfdcf3b99d8b9daea103199d4e1b7cb
zhenia-chugaev <zhenia.chugaev@bk.ru>
2022-02-06 20:45:42 +0300

    Add basic functionality to the executable

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
+import { ask } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+
+const userName = ask('May I have your name?');
+console.log(`Hello, ${userName}!`);
